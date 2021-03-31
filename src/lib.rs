89298c26a016d5facde4a9426bbac91c9d4da9be
tvallotton <tvallotton@uc.cl>
2021-03-31 14:05:24 -0300

    fixing version in docs

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@
 //! To use `rocket_auth` include it as a dependency in your Cargo.toml file: 
 //! ```ini
 //! [dependencies.rocket_auth]
-//! version = "0.1.3"
+//! version = "0.2.0"
 //! features = ["sqlite-db"]
 //! ```
 //! # Quick overview
