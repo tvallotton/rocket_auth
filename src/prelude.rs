db5561199e190b2b7a4089bc901940eca24b5c17
tvallotton <tvallotton@uc.cl>
2021-07-01 01:42:15 -0400

    Sorting imports in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,18 +1,14 @@
 // pub use crate::language::Language;
 pub use crate::cookies::Session;
-pub(crate) use crate::db::DBConnection;
-pub(crate) use crate::error::Error;
+pub use crate::error::Error;
 pub use crate::session::SessionManager;
 pub use crate::{Login, Signup, User, Users};
-pub use crate::error::Error;
 
+pub(crate) use crate::db::DBConnection;
+pub(crate) use crate::error::Error;
 pub(crate) use serde::{Deserialize, Serialize};
-
 pub(crate) use std::ops::Deref;
 pub(crate) use std::time::Duration;
 pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
 pub(crate) use async_trait::async_trait;
-
-
-
 pub(crate) use fehler::*;
