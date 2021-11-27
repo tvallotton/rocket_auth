c74f55f1604d68791e0b6cd7d65bf9e615718a67
tvallotton <tvallotton@uc.cl>
2021-11-26 22:38:37 -0300

    making redis impl async

--- src/session/redis/mod.rs
+++ src/session/redis/mod.rs
@@ -1,38 +1,42 @@
 use super::SessionManager;
 use crate::prelude::*;
 
-use redis::{Client, Commands};
+use redis::{AsyncCommands, Client};
 
 const YEAR_IN_SECS: usize = 365 * 60 * 60 * 24;
 
 #[async_trait]
 impl SessionManager for Client {
     async fn insert(&self, id: i32, key: String) -> Result {
-        let mut cnn = self.get_connection()?;
-        cnn.set_ex(id, key, YEAR_IN_SECS)?;
+        let mut cnn = self.get_async_connection().await?;
+        cnn.set_ex(id, key, YEAR_IN_SECS).await?;
+
         Ok(())
     }
 
     async fn insert_for(&self, id: i32, key: String, time: Duration) -> Result {
-        let mut cnn = self.get_connection()?;
-        cnn.set_ex(id, key, time.as_secs() as usize)?;
+        let mut cnn = self.get_async_connection().await?;
+        cnn.set_ex(id, key, time.as_secs() as usize).await?;
         Ok(())
     }
 
     async fn remove(&self, id: i32) -> Result {
-        let mut cnn = self.get_connection()?;
-        cnn.del(id)?;
+        let mut cnn = self.get_async_connection().await?;
+        cnn.del(id).await?;
         Ok(())
     }
 
     async fn get(&self, id: i32) -> Option<String> {
-        let mut cnn = self.get_connection().ok()?;
-        cnn.get(id).ok()
+        let mut cnn = self.get_async_connection().await.ok()?;
+        cnn.get(id).await.ok()
     }
 
     async fn clear_all(&self) -> Result {
-        let mut cnn = self.get_connection()?;
-        redis::Cmd::new().arg("FLUSHDB").execute(&mut cnn);
+        let mut cnn = self.get_async_connection().await?;
+        redis::Cmd::new()
+            .arg("FLUSHDB")
+            .query_async::<_, ()>(&mut cnn)
+            .await?;
         Ok(())
     }
 
