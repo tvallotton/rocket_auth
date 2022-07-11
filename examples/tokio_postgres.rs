45846595267e6a41136efb3cdcc4adda6ae14d56
tvallotton <tvallotton@uc.cl>
2022-07-11 00:07:04 -0400

    fix: tokio postgres examples and signup
    tokio postgres examples weren't updated last time. Additionally,
    fixed signup which did not cast tokio-postgres errors into validation errors

--- examples/tokio_postgres.rs
+++ examples/tokio_postgres.rs
@@ -2,10 +2,12 @@ use rocket::{form::*, get, post, response::Redirect, routes, State};
 use rocket_auth::{prelude::Error, *};
 use rocket_dyn_templates::Template;
 use serde_json::json;
+use std::convert::TryInto;
+use tokio_postgres::{connect, Client, NoTls};
 
+use std::result::Result;
+use std::sync::Arc;
 use std::*;
-use std::{convert::TryInto, result::Result};
-use tokio_postgres::{connect, Client};
 #[get("/login")]
 fn get_login() -> Template {
     Template::render("login", json!({}))
@@ -25,9 +27,9 @@ async fn get_signup() -> Template {
 
 #[post("/signup", data = "<form>")]
 async fn post_signup(auth: Auth<'_>, form: Form<Signup>) -> Result<Redirect, Error> {
-    auth.signup(&form).await?;
-    auth.login(&form.into()).await?;
+    auth.signup(&form).await.map_err(|err| dbg!(err))?;
 
+    auth.login(&form.into()).await?;
     Ok(Redirect::to("/"))
 }
 
@@ -36,23 +38,21 @@ async fn index(user: Option<User>) -> Template {
     Template::render("index", json!({ "user": user }))
 }
 
-#[get("/logout")]
-async fn logout(auth: Auth<'_>) -> Result<Template, Error> {
+#[post("/logout")]
+async fn post_logout(auth: Auth<'_>) -> Result<Template, Error> {
     auth.logout().await?;
     Ok(Template::render("logout", json!({})))
 }
-#[get("/delete")]
+
+#[post("/delete")]
 async fn delete(auth: Auth<'_>) -> Result<Template, Error> {
     auth.delete().await?;
     Ok(Template::render("deleted", json!({})))
 }
 
 #[get("/show_all_users")]
-async fn show_all_users(
-    client: &State<sync::Arc<Client>>,
-    user: Option<User>,
-) -> Result<Template, Error> {
-    let users: Vec<User> = client
+async fn show_all_users(conn: &State<Arc<Client>>, user: Option<User>) -> Result<Template, Error> {
+    let users: Vec<User> = conn
         .query("select * from users;", &[])
         .await?
         .into_iter()
@@ -65,38 +65,71 @@ async fn show_all_users(
     ))
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    use tokio_postgres::NoTls;
-    let (client, conn) = connect("host=localhost user=postgres", NoTls).await?;
-    let client = sync::Arc::new(client);
-    let users: Users = client.clone().into();
-
+#[rocket::launch]
+async fn rocket() -> _ {
+    let (client, conn) = connect("host=localhost user=tomasvallotton", NoTls)
+        .await
+        .unwrap();
     tokio::spawn(async move {
         if let Err(e) = conn.await {
-            eprintln!("TokioPostgresError: {}", e);
+            panic!("{}", e);
         }
     });
-    users.create_table().await?;
-    let _ = rocket::build()
+    let client = Arc::new(client);
+    let users: Users = client.clone().into();
+    users.create_table().await.unwrap();
+
+    rocket::build()
         .mount(
             "/",
             routes![
                 index,
                 get_login,
                 post_signup,
+                post_logout,
                 get_signup,
                 post_login,
-                logout,
                 delete,
-                show_all_users
+                show_all_users,
             ],
         )
         .manage(client)
         .manage(users)
         .attach(Template::fairing())
-        .launch()
-        .await
-        .unwrap();
-    Ok(())
 }
+
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     use tokio_postgres::NoTls;
+//     let (client, conn) = connect("host=localhost user=tomasvallotton", NoTls).await?;
+//     let client = sync::Arc::new(client);
+//     let users: Users = client.clone().into();
+
+// tokio::spawn(async move {
+//     if let Err(e) = conn.await {
+//        panic!("{e}")
+//     }
+// });
+//     users.create_table().await?;
+//     let _ = rocket::build()
+//         .mount(
+//             "/",
+//             routes![
+//                 index,
+//                 get_login,
+//                 post_signup,
+//                 get_signup,
+//                 post_login,
+//                 logout,
+//                 delete,
+//                 show_all_users
+//             ],
+//         )
+//         .manage(client)
+//         .manage(users)
+//         .attach(Template::fairing())
+//         .launch()
+//         .await
+//         .unwrap();
+//     Ok(())
+// }
